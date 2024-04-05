ef0eacd0fe8faa0dbcc3b8ca708957426a35407e
Ryan Ali <ryanali456@gmail.com>
2024-04-06 03:38:03 +0800

    create initial migration

---
+++ EventManagementApp/Migrations/20240405191007_CreateLoginCredentialTable.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventManagementApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateLoginCredentialTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(name:"LoginCredential", columns: table => new {
+                Id = table.Column<Guid>(type: "uniqueidentifier", defaultValueSql: "NEWID()"),
+                Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                IsRoot = table.Column<bool>(type: "bit", defaultValue: false),
+                CreatedAt = table.Column<DateTime>(type: "datetime", defaultValueSql: "GETDATE()")
+            },
+            constraints: table =>{
+                table.PrimaryKey("PK_LoginCredential", row=>row.Id);
+            }
+           );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name:"LoginCredential");
+        }
+    }
+}
---
+++ EventManagementApp/Migrations/20240405192548_CreateUserTable.cs
@@ -0,0 +1,35 @@
+﻿using EventManagementApp.Services;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventManagementApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable("User", columns: table=> new {
+                Id = table.Column<Guid>(type: "UNIQUEIDENTIFIER", defaultValueSql: "NEWID()"),
+                GivenName = table.Column<string>(type:"NVARCHAR(50)", nullable:false),
+                MiddleName = table.Column<string>(type:"NVARCHAR(50)", nullable:false),
+                Surname = table.Column<string>(type:"NVARCHAR(50)", nullable:false),
+                CreatedAt = table.Column<DateTime>(type: "DATETIME", defaultValueSql: "GETDATE()"),
+                DeletedAt = table.Column<DateTime>(type: "DATETIME", nullable: true),
+                LoginCredentialId = table.Column<Guid>(type: "UNIQUEIDENTIFIER")
+            }, constraints : table => {
+                table.PrimaryKey("PK_User", row=> row.Id);
+                table.ForeignKey(name: "FK_User_LoginCredential_LoginCredentialId", column: row=> row.LoginCredentialId, 
+                principalTable:"LoginCredential", principalColumn: "Id");
+            });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable("User");
+        }
+    }
+}
