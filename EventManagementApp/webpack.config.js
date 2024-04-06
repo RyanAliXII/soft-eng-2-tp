4e0173a92c14556335376a0bb2216a2a27880fb8
Ryan Ali <ryanali456@gmail.com>
2024-04-07 05:09:57 +0800

    remove unused files

--- EventManagementApp/webpack.config.js
+++ EventManagementApp/webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
       {
         test: /\.ts?$/,
---
+++ EventManagementApp/wwwroot/src/ts/test.ts
@@ -0,0 +1,3 @@
+import { v1 } from "uuid";
+
+console.log(v1());
