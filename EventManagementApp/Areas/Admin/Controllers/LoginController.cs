397868baf80b77a9444ada5ff56b624dcb2dc8b7
Ryan Ali <ryanali456@gmail.com>
2024-04-06 15:37:46 +0800

    create login view model and login endpoint

--- EventManagementApp/Areas/Admin/Controllers/LoginController.cs
+++ EventManagementApp/Areas/Admin/Controllers/LoginController.cs
@@ -1,3 +1,4 @@
+using EventManagementApp.Areas.Admin.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EventManagementApp.Areas.Admin.Controllers
@@ -7,6 +8,11 @@ namespace EventManagementApp.Areas.Admin.Controllers
     {
         public ActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginViewModel creds){
+        
             return View();
         }
 
---
+++ EventManagementApp/Areas/Admin/ViewModels/LoginViewModel.cs
@@ -0,0 +1,6 @@
+
+namespace EventManagementApp.Areas.Admin.ViewModels;
+public class LoginViewModel {
+    public string Email = string.Empty;
+    public string Password = string.Empty;
+}
