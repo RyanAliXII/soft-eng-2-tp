5f145661368ae1b8bb073aac8d05ab9663aa2ae9
Ryan Ali <ryanali456@gmail.com>
2024-04-17 08:10:49 +0800

    handle date input

--- EventManagementApp/wwwroot/src/ts/create-event.ts
+++ EventManagementApp/wwwroot/src/ts/create-event.ts
@@ -1,7 +1,37 @@
 import { createApp } from "vue";
-
+import { useForm } from "./composables/useForm";
+import { Event } from "./types/event";
+import { object, string } from "zod";
+import { isValidDatetime, toISO8601DateString } from "./utils/datetime";
+type CreateEventForm = Omit<Event, "id">;
 createApp({
   setup() {
-    return {};
+    const { form, errors } = useForm<CreateEventForm>({
+      initialValue: {
+        name: "",
+        activities: [],
+        date: new Date(),
+      },
+      schema: object({
+        name: string(),
+      }),
+    });
+    const handleDateInput = (event: InputEvent) => {
+      const target = event.target as HTMLInputElement;
+      const value = target.value;
+      try {
+        const date = new Date(value);
+        form.value.date = date;
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    return {
+      form,
+      errors,
+      toISO8601DateString,
+      handleDateInput,
+    };
   },
 }).mount("#createEventPage");
